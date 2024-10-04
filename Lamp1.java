d17c3dd3a927ab93879a3180b910df0252eb16aa
SarmientoMarvin11145 <marvinsarmiento847@gmail.com>
2024-10-04 14:04:26 +0800

    Create Lamp1.java

---
+++ Lamp1.java
@@ -0,0 +1,14 @@
+class Lamp1
+{
+  boolean isOn;
+
+  void turnOn(){
+    isOn = true;
+    System.out.println("Light On?" + isOn);
+  }
+  
+  void turnOff(){
+    isOn = false;
+    System.out.println("Light Off?" + isOn);
+  }
+}  
