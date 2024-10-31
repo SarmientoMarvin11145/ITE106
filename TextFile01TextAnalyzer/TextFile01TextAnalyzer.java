21f2439325a022c894b08976b0f416eecbc0b232
Marvin Sarmiento <marvinsarmiento847@gmail.com>
2024-10-31 23:24:12 +0800

    Update TextFile01TextAnalyzer.java

--- TextFile01TextAnalyzer/TextFile01TextAnalyzer.java
+++ TextFile01TextAnalyzer/TextFile01TextAnalyzer.java
@@ -4,7 +4,7 @@ import java.io.FileReader;
 
 public class TextAnalyzer {
     public static void main(String[] args) {
-        File file = new File("textfile.txt");
+        File file = new File("Marvin.txt");
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
