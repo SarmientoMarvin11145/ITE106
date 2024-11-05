925f56ca789cf029827f59de71141723ed2cd463
Marvin Sarmiento <marvinsarmiento847@gmail.com>
2024-11-05 18:00:29 +0800

    Create TestInheritance.java

---
+++ Bicycle/TestInheritance.java
@@ -0,0 +1,10 @@
+//driver class
+public class Test Inheritance 
+{
+  public static void main(String [] args) 
+  {
+    MountainBike mb = new MountainBike(3, 100, 25);
+    System.out.println(mb.toString());
+  }
+}
+    
